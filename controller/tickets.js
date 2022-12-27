5b9179186bf83c07d6c8651264785b41367961cb
Isaac Newton Tingo <newtone480@gmail.com>
2022-12-27 15:44:39 +0300

    Ticket purchase

--- controller/tickets.js
+++ controller/tickets.js
@@ -1,6 +1,9 @@
 const { Event } = require("../models/events");
 const { Ticket } = require("../models/tickets");
 
+const { v4: uuidv4 } = require("uuid");
+const request = require("request");
+
 exports.createTicket = async (req, res) => {
   try {
     const { event, ticketType, ticketPrice, numberOfTickets } = req.body;
@@ -97,6 +100,107 @@ exports.getTicketVariants = async (req, res) => {
     res.json({
       status: "Failed",
       message: "Something went wrong while getting event tickets",
+    });
+  }
+};
+
+//buy a ticket
+exports.buyEventTicket = async (req, res) => {
+  try {
+    const eventTicketID = req.params.id;
+    let { phoneNumber, userID } = req.body;
+    const accountNumber = uuidv4() + userID;
+
+    const ticket = await Ticket.findOne({ _id: eventTicketID }).populate(
+      "event"
+    );
+
+    if (ticket.event.active === true) {
+      //make payment via mpesa
+      const amount = ticket.ticketPrice;
+      const body = `amount=${amount}&msisdn=${parseInt(
+        phoneNumber
+      )}&account_no=${accountNumber}`;
+
+      request(
+        {
+          url: "https://tinypesa.com/api/v1/express/initialize",
+          method: "POST",
+          headers: {
+            Apikey: process.env.APE_30_TINY_PESA_API_KEY,
+            "Content-Type": "application/x-www-form-urlencoded",
+          },
+          body: body,
+        },
+        function (error, response, body) {
+          if (error) {
+            console.log(error);
+          } else {
+            const sendRes = JSON.parse(body);
+
+            if (sendRes.success === true) {
+              //check payment
+              let paymentStatus = 0;
+              const interval = setInterval(() => {
+                console.log("---------Checking payment---------");
+                request(
+                  {
+                    url: `https://tinypesa.com/api/v1/express/get_status/${accountNumber}`,
+                    method: "GET",
+                    headers: {
+                      Apikey: process.env.APE_30_TINY_PESA_API_KEY,
+                      Accept: "application/json",
+                    },
+                  },
+                  function (error, response, body) {
+                    if (error) {
+                      console.log(error);
+                    } else {
+                      const newBody = JSON.parse(body);
+                      paymentStatus = newBody.is_complete;
+                      if (newBody.is_complete === 1) {
+                        clearInterval(interval);
+                        clearTimeout(timeOut);
+
+                        res.json({
+                          status: "Success",
+                          message: "Payment made successfully",
+                        });
+                      }
+                    }
+                  }
+                );
+              }, 1000);
+
+              const timeOut = setTimeout(() => {
+                clearInterval(interval);
+
+                res.json({
+                  status: "Failed",
+                  message:
+                    "You did not complete the payment process. Please make sure you are next to your phone and make the payment",
+                });
+              }, 120000);
+            } else {
+              res.json({
+                status: "Error",
+                ResponseDescription: "An error occured. Please try again later",
+              });
+            }
+          }
+        }
+      );
+    } else {
+      res.json({
+        status: "Failed",
+        message: "Event has expired",
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    res.json({
+      status: "Failed",
+      message: "Something went wrong while buying the ticket",
     });
   }
 };
--- routes/tickets.js
+++ routes/tickets.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const router = express.Router();
 
-const { createTicket, getTicketVariants } = require("../controller/tickets");
+const {
+  createTicket,
+  getTicketVariants,
+  buyEventTicket,
+} = require("../controller/tickets");
 
 //Only admin can add these tickets
 router.post("/create-ticket", createTicket);
+
+//Users
 router.get("/get-event-tickets/:id", getTicketVariants);
+router.post("/buy-event-ticket/:id", buyEventTicket);
 
 module.exports = router;
